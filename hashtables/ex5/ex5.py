5cadeba0234b4b47332f70759a75aa610b42279f
Eric Wood <epwood1@gmail.com>
2020-09-13 19:56:45 -0700

    mvp and stretch

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,6 +7,22 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    files_dict = {}
+    result = []
+    for f in files:
+        if f[-1] == '/':
+            continue
+
+        sfiles = f.split('/')
+        if sfiles[-1] in files_dict:
+            files_dict[sfiles[-1]].append(f)
+        else:
+            files_dict[sfiles[-1]] = [f]
+
+    for q in queries:
+        if q in files_dict:
+            for i in files_dict[q]:
+                result.append(i)
 
     return result
 
